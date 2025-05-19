3fea0150eb2ff29e9d37a38fe87038a8e4a9c3b2
LeHAhn <151824349+LeHAhn@users.noreply.github.com>
2025-05-19 16:56:31 +0700

    Update UC_Home.Designer.cs

--- MayTinhQA/UserControls/UC_Home.Designer.cs
+++ MayTinhQA/UserControls/UC_Home.Designer.cs
@@ -29,11 +29,11 @@
             // 
             this.labelName.AutoSize = true;
             this.labelName.Font = new System.Drawing.Font("Montserrat", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelName.Location = new System.Drawing.Point(599, 286);
+            this.labelName.Location = new System.Drawing.Point(310, 247);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(84, 25);
+            this.labelName.Size = new System.Drawing.Size(613, 25);
             this.labelName.TabIndex = 4;
-            this.labelName.Text = "Chào A!";
+            this.labelName.Text = "Phần mềm Quản lý quan hệ khách hàng của công ty Quang Anh";
             // 
             // UC_Home
             // 
